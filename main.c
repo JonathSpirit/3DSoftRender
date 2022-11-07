69cadad34286bb237886e915d8f7891d357d6881
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2022-11-07 17:19:02 +0100

    implementing triangle drawing WIP

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct
 
 typedef struct
 {
-    unsigned char _w, _h;
+    uint16_t _w, _h;
 } ScreenSize;
 
 typedef struct
@@ -19,6 +19,7 @@ typedef struct
     Coord3 _pos;
     float _scale;
     ScreenSize _screenSize;
+    uint8_t* _screenData;
 } Camera;
 
 typedef struct
@@ -29,21 +30,32 @@ typedef struct
 
 typedef struct
 {
-    Coord3* _points;
-    Index _pointsSize;
-
-    Line3* _lines;
-    Index _linesSize;
-
-    Coord3 _center;
-} Object3D;
-
-typedef struct
-{
     float _row1[3];
     float _row2[3];
     float _row3[3];
 } Matrix3x3;
+
+typedef struct
+{
+    Coord3 _v0;
+    Coord3 _v1;
+    Coord3 _v2;
+    Coord3 _normal;
+} Triangle;
+
+typedef struct
+{
+    Coord3 _origin;
+    Coord3 _direction;
+} Ray;
+
+typedef struct
+{
+    Triangle* _triangles;
+    Index _trianglesSize;
+
+    Coord3 _center;
+} Object3D;
 
 //https://people.eecs.ku.edu/~jrmiller/Courses/VectorGeometry/VectorOperations.html
 float GetMagnitudeFromCoord(Coord3* a)
@@ -75,6 +87,99 @@ float GetAngleBetween2Coord(Coord3* a, Coord3* b)
     return atan2f( GetMagnitudeFromCoord(&crossResult), GetDotProductFrom2Coord(a, b) );
     //return Math.Atan2(Cross(A,B), Dot(A,B));
 }
+void Normalize(Coord3* a)
+{
+    float mag = GetMagnitudeFromCoord(a);
+    a->_x /= mag;
+    a->_y /= mag;
+    a->_z /= mag;
+}
+
+uint8_t RayIntersect(Ray* ray, Triangle* triangle, Coord3* intersectPoint)
+{
+    float dotParallel = GetDotProductFrom2Coord(&triangle->_normal, &ray->_direction);
+    const float kEpsilon = 1e-8f;
+
+    //The Ray And The Triangle Are Parallel
+    if (fabsf(dotParallel) < kEpsilon)
+    {
+        return 0;
+    }
+
+    float planeDistance = GetDotProductFrom2Coord(&triangle->_normal, &triangle->_v0);
+    float distance = - ((GetDotProductFrom2Coord(&triangle->_normal, &ray->_origin) + planeDistance) / dotParallel);
+
+    //The triangle is "behind" the ray
+    if (distance < 0.0f)
+    {
+        return 0;
+    }
+
+    // compute the intersection point
+    intersectPoint->_x = -(ray->_origin._x + distance*ray->_direction._x);
+    intersectPoint->_y = -(ray->_origin._y + distance*ray->_direction._y);
+    intersectPoint->_z = -(ray->_origin._z + distance*ray->_direction._z);
+
+    Coord3 edge;
+    Coord3 C;
+    Coord3 vp;
+
+    //edge 0
+    edge._x = triangle->_v1._x - triangle->_v0._x;
+    edge._y = triangle->_v1._y - triangle->_v0._y;
+    edge._z = triangle->_v1._z - triangle->_v0._z;
+
+    vp._x = intersectPoint->_x - triangle->_v0._x;
+    vp._y = intersectPoint->_y - triangle->_v0._y;
+    vp._z = intersectPoint->_z - triangle->_v0._z;
+
+    GetCrossProductFrom2Coord(&edge, &vp, &C);
+
+    if (GetDotProductFrom2Coord(&triangle->_normal, &C) < 0.0f)
+    {
+        return 0;
+    }
+
+    //edge 1
+    edge._x = triangle->_v2._x - triangle->_v1._x;
+    edge._y = triangle->_v2._y - triangle->_v1._y;
+    edge._z = triangle->_v2._z - triangle->_v1._z;
+
+    vp._x = intersectPoint->_x - triangle->_v1._x;
+    vp._y = intersectPoint->_y - triangle->_v1._y;
+    vp._z = intersectPoint->_z - triangle->_v1._z;
+
+    GetCrossProductFrom2Coord(&edge, &vp, &C);
+
+    if (GetDotProductFrom2Coord(&triangle->_normal, &C) < 0.0f)
+    {
+        return 0;
+    }
+
+    //edge 2
+    edge._x = triangle->_v0._x - triangle->_v2._x;
+    edge._y = triangle->_v0._y - triangle->_v2._y;
+    edge._z = triangle->_v0._z - triangle->_v2._z;
+
+    vp._x = intersectPoint->_x - triangle->_v2._x;
+    vp._y = intersectPoint->_y - triangle->_v2._y;
+    vp._z = intersectPoint->_z - triangle->_v2._z;
+
+    GetCrossProductFrom2Coord(&edge, &vp, &C);
+
+    if (GetDotProductFrom2Coord(&triangle->_normal, &C) < 0.0f)
+    {
+        return 0;
+    }
+
+    // implementing the single/double sided feature
+    /*if (GetDotProductFrom2Coord(&ray->_direction, &triangle->_normal) > 0.0f)
+    {
+        return 0;
+    }*/
+
+    return 1;
+}
 
 void SetSurfacePixel(SDL_Surface* surface, int x, int y, SDL_Color color)
 {
@@ -93,8 +198,76 @@ void SetSurfacePixel(SDL_Surface* surface, int x, int y, SDL_Color color)
     *target_pixel = SDL_MapRGBA(surface->format, color.r, color.g, color.b, color.a);
 }
 
+void SetPixel(Camera* camera, uint16_t x, uint16_t y, uint8_t r, uint8_t g,  uint8_t b)
+{
+    if (x>=camera->_screenSize._w || y>=camera->_screenSize._h)
+    {
+        return;
+    }
+
+    uint8_t* const pixelData = (camera->_screenData
+                                + ((uint32_t)y * camera->_screenSize._w + (uint32_t)x) * 3);
+    *pixelData = r;
+    *(pixelData+1) = g;
+    *(pixelData+2) = b;
+}
+
+void ToSurfacePixels(SDL_Surface* surface, Camera* camera)
+{
+    if (surface->w == camera->_screenSize._w &&
+        surface->h == camera->_screenSize._h)
+    {
+        uint16_t x, y;
+        uint8_t* pixelData = camera->_screenData;
+
+        for (y=0; y<camera->_screenSize._h; ++y)
+        {
+            for (x=0; x<camera->_screenSize._w; ++x)
+            {
+                SetSurfacePixel(surface, x, y, (SDL_Color){*pixelData, *(pixelData+1), *(pixelData+2), 255});
+                pixelData += 3;
+            }
+        }
+    }
+}
+
+void ClearCamera(Camera* camera)
+{
+    uint32_t size = camera->_screenSize._w * camera->_screenSize._h * 3;
+    uint32_t i;
+
+    for (i=0; i<size; ++i)
+    {
+        camera->_screenData[i] = 0x00;
+    }
+}
+
+float Deg2rad(float deg)
+{
+    return deg * ((float)M_PI / 180.0f);
+}
+
+void GetTriangleNormal(Triangle* triangle)
+{
+    Coord3 A;
+    Coord3 B;
+
+    //v1 - v0
+    A._x = triangle->_v1._x - triangle->_v0._x;
+    A._y = triangle->_v1._y - triangle->_v0._y;
+    A._z = triangle->_v1._z - triangle->_v0._z;
+
+    //v2 - v0
+    B._x = triangle->_v2._x - triangle->_v0._x;
+    B._y = triangle->_v2._y - triangle->_v0._y;
+    B._z = triangle->_v2._z - triangle->_v0._z;
+
+    GetCrossProductFrom2Coord(&A, &B, &triangle->_normal);
+    Normalize(&triangle->_normal);
+}
+
 uint8_t r,g,b;
-void DrawPoint(SDL_Surface* surface, Coord3* coord, Camera* camera)
+void DrawPoint(Coord3* coord, Camera* camera)
 {
     static Coord3 axeXp = {1.0f, 0.0f, 0.0f};
     static Coord3 axeYp = {0.0f, 1.0f, 0.0f};
@@ -109,49 +282,55 @@ void DrawPoint(SDL_Surface* surface, Coord3* coord, Camera* camera)
     angle = GetAngleBetween2Coord(&axeYp, coord);
     resulty = cosf(angle)*mag;
 
+    SetPixel(camera, (uint16_t)resultx, (uint16_t)resulty, r, g, b);
+
     //printf("Drawing at x:%d y:%d with color r:%u g:%u b:%u\n", (int)resultx, (int)resulty, r, g, b);
-    SetSurfacePixel(surface, (int)resultx, (int)resulty, (SDL_Color){r, g, b, 255});
+    //SetSurfacePixel(surface, (int)resultx, (int)resulty, (SDL_Color){r, g, b, 255});
 }
-void DrawObject3D(SDL_Surface* surface, Object3D* object, Camera* camera)
+void DrawTriangle(Triangle* triangle, Camera* camera)
 {
-    Index i, points;
+    Ray ray;
+    uint16_t x, y;
+    uint8_t intersect;
+    Coord3 intersectPoint;
 
-    float angle;
-    float mag, distance;
-    float resultx, resulty;
+    ray._origin._x = 0.0f;
+    ray._origin._y = 0.0f;
+    ray._origin._z = 800.0f;
 
-    Coord3 coordNormal, coordA, coordB;
+    float fov = 51.52f;
+    float scale = tanf(Deg2rad(fov * 0.5f));
+    float imageAspectRatio = (float)camera->_screenSize._w / (float)camera->_screenSize._h;
 
-    static Coord3 axeXp = {1.0f, 0.0f, 0.0f};
-    static Coord3 axeYp = {0.0f, 1.0f, 0.0f};
+    GetTriangleNormal(triangle);
 
-    //Drawing points
-    /*for (i=0; i<object->_pointsSize; ++i)
+    for (y=0; y<camera->_screenSize._h; ++y)
     {
-        DrawPoint(surface, &object->_points[i], camera);
-    }*/
-
-    //Drawing lines
-    for (i=0; i<object->_linesSize; ++i)
-    {
-        coordA = object->_points[ object->_lines[i]._pointA ];
-        coordB = object->_points[ object->_lines[i]._pointB ];
-
-        mag = GetDistanceBetween2Coord(&coordA, &coordB);
-
-        coordNormal._x = (coordA._x - coordB._x)/mag;
-        coordNormal._y = (coordA._y - coordB._y)/mag;
-        coordNormal._z = (coordA._z - coordB._z)/mag;
-
-        distance = mag / 500.0f;
-        for (points=0; points<501; ++points)
+        for (x=0; x<camera->_screenSize._w; ++x)
         {
-            coordB._x = coordA._x + coordNormal._x*-distance*(float)points;
-            coordB._y = coordA._y + coordNormal._y*-distance*(float)points;
-            coordB._z = coordA._z + coordNormal._z*-distance*(float)points;
+            // compute primary ray
+            ray._direction._x = (2 * ((float)x + 0.5f) / (float)camera->_screenSize._w - 1) * imageAspectRatio * scale;
+            ray._direction._y = (1 - 2 * ((float)y + 0.5f) / (float)camera->_screenSize._h) * scale;
+            ray._direction._z = -1.0f;
+            Normalize(&ray._direction);
 
-            DrawPoint(surface, &coordB, camera);
+            intersect = RayIntersect(&ray, triangle, &intersectPoint);
+
+            float distance = 1.0f - GetDistanceBetween2Coord(&ray._origin, &intersectPoint) / 1000.0f;
+
+            if (intersect == 1)
+            {
+                SetPixel(camera, x,y, (uint8_t)(255.0f*distance),0,0);
+            }
         }
+    }
+}
+void DrawObject3D(Object3D* object, Camera* camera)
+{
+    Index i;
+    for (i=0; i<object->_trianglesSize; ++i)
+    {
+        DrawTriangle(object->_triangles+i, camera);
     }
 }
 
@@ -197,9 +376,11 @@ void ApplyMatrixToObject(Matrix3x3* matrix, Object3D* object)
     Coord3 newPos;
     Index i;
 
-    for (i=0; i<object->_pointsSize; ++i)
+    //printf("------------------------\n");
+    for (i=0; i<object->_trianglesSize; ++i)
     {
-        oldPos = object->_points[i];
+        //v0
+        oldPos = object->_triangles[i]._v0;
         oldPos._x -= object->_center._x;
         oldPos._y -= object->_center._y;
         oldPos._z -= object->_center._z;
@@ -209,22 +390,53 @@ void ApplyMatrixToObject(Matrix3x3* matrix, Object3D* object)
         newPos._y += object->_center._y;
         newPos._z += object->_center._z;
 
-        object->_points[i] = newPos;
+        object->_triangles[i]._v0 = newPos;
+
+        //printf("x: %f.2 y: %f.2 z: %f.2\n", newPos._x, newPos._y, newPos._z);
+
+        //v1
+        oldPos = object->_triangles[i]._v1;
+        oldPos._x -= object->_center._x;
+        oldPos._y -= object->_center._y;
+        oldPos._z -= object->_center._z;
+
+        MultiplyMatrixWithCoord(matrix, &oldPos, &newPos);
+        newPos._x += object->_center._x;
+        newPos._y += object->_center._y;
+        newPos._z += object->_center._z;
+
+        object->_triangles[i]._v1 = newPos;
+
+        //v2
+        oldPos = object->_triangles[i]._v2;
+        oldPos._x -= object->_center._x;
+        oldPos._y -= object->_center._y;
+        oldPos._z -= object->_center._z;
+
+        MultiplyMatrixWithCoord(matrix, &oldPos, &newPos);
+        newPos._x += object->_center._x;
+        newPos._y += object->_center._y;
+        newPos._z += object->_center._z;
+
+        object->_triangles[i]._v2 = newPos;
     }
 }
 
-Coord3 monCubePoints[8] = {{0.0f,0.0f,0.0f}, {20.0f,0.0f,0.0f}, {0.0f, 0.0f, 20.0f}, {20.0f, 0.0f, 20.0f},
-                           {0.0f,20.0f,0.0f}, {20.0f,20.0f,0.0f}, {0.0f, 20.0f, 20.0f}, {20.0f, 20.0f, 20.0f}};
+Triangle monCubeTriangles[12] = {
+        {{0.0f,0.0f,0.0f}, {20.0f,0.0f,0.0f}, {0.0f,20.0f,0.0f}}, {{20.0f,20.0f,0.0f}, {20.0f,0.0f,0.0f}, {0.0f,20.0f,0.0f}},
+        {{0.0f,0.0f,20.0f}, {20.0f,0.0f,20.0f}, {0.0f,20.0f,20.0f}}, {{20.0f,20.0f,20.0f}, {20.0f,0.0f,20.0f}, {0.0f,20.0f,20.0f}},
 
-Line3 monCubeLines[12] = {{0,1}, {0,2}, {0,4},
-                          {7,6}, {7,5}, {7,3},
-                          {3,2}, {3,1},
-                          {4,6}, {4,5},
-                          {6,2}, {5,1}};
+        {{0.0f,20.0f,0.0f}, {20.0f,20.0f,0.0f}, {0.0f,20.0f,20.0f}}, {{20.0f,20.0f,20.0f}, {20.0f,20.0f,0.0f}, {0.0f,20.0f,20.0f}},
+        {{0.0f,0.0f,0.0f}, {20.0f,0.0f,0.0f}, {0.0f,0.0f,20.0f}}, {{20.0f,0.0f,20.0f}, {20.0f,0.0f,0.0f}, {0.0f,0.0f,20.0f}},
 
-Object3D monCube = {monCubePoints, 8, monCubeLines, 12, {10.0f,10.0f,10.0f}};
+        {{0.0f,0.0f,0.0f}, {0.0f,0.0f,20.0f}, {0.0f,20.0f,0.0f}}, {{0.0f,20.0f,20.0f}, {0.0f,0.0f,20.0f}, {0.0f,20.0f,0.0f}},
+        {{20.0f,0.0f,0.0f}, {20.0f,0.0f,20.0f}, {20.0f,20.0f,0.0f}}, {{20.0f,20.0f,20.0f}, {20.0f,0.0f,20.0f}, {20.0f,20.0f,0.0f}}
+};
 
-Camera camera = {{0.0f,0.0f,0.0f}, 1.0f, {64,32}};
+Object3D monCube = {(Triangle*)&monCubeTriangles, sizeof(monCubeTriangles) / sizeof(Triangle), {10.0f, 10.0f, 10.0f}};
+
+uint8_t screen[640*320*3];
+Camera camera = {{0.0f,0.0f,0.0f}, 1.0f, {640,320}, (uint8_t*)&screen};
 
 Matrix3x3 rotationMatrixZ, rotationMatrixX;
 
@@ -235,7 +447,7 @@ int main(int argc, char *argv[])
     unsigned char count=0;
 
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Event event;
 
@@ -251,26 +463,58 @@ int main(int argc, char *argv[])
     CreateRotationMatrixOnAxeX(&rotationMatrixX, 0.087f);
 
     //Translating
-    for (i=0; i<monCube._pointsSize; ++i)
+    for (i=0; i<monCube._trianglesSize; ++i)
     {
-        coord = monCube._points[i];
-        coord._x += 22.0f;
-        coord._y += 6.0f;
-        coord._z += 0.0f;
-
+        coord = monCube._triangles[i]._v0;
         coord._x *= 10.0f;
         coord._y *= 10.0f;
         coord._z *= 10.0f;
 
-        monCube._points[i] = coord;
-    }
-    monCube._center._x += 22.0f;
-    monCube._center._y += 6.0f;
-    monCube._center._z += 0.0f;
+        /*coord._x += 400.0f;
+        coord._y += 100.0f;
+        coord._z += 0.0f;*/
 
+        monCube._triangles[i]._v0 = coord;
+
+        coord = monCube._triangles[i]._v1;
+        coord._x *= 10.0f;
+        coord._y *= 10.0f;
+        coord._z *= 10.0f;
+
+        /*coord._x += 400.0f;
+        coord._y += 100.0f;
+        coord._z += 0.0f;*/
+
+        monCube._triangles[i]._v1 = coord;
+
+        coord = monCube._triangles[i]._v2;
+        coord._x *= 10.0f;
+        coord._y *= 10.0f;
+        coord._z *= 10.0f;
+
+        /*coord._x += 400.0f;
+        coord._y += 100.0f;
+        coord._z += 0.0f;*/
+
+        monCube._triangles[i]._v2 = coord;
+    }
     monCube._center._x *= 10.0f;
     monCube._center._y *= 10.0f;
     monCube._center._z *= 10.0f;
+
+    /*monCube._center._x += 400.0f;
+    monCube._center._y += 100.0f;
+    monCube._center._z += 0.0f;*/
+
+    Triangle testTriangle;
+    testTriangle._v0 = (Coord3){0.0f, 0.0f, 0.0f};
+    testTriangle._v1 = (Coord3){30.0f, 0.0f, 0.0f};
+    testTriangle._v2 = (Coord3){0.0f, 30.0f, 0.0f};
+
+    Triangle zeroTriangle;
+    zeroTriangle._v0 = (Coord3){-20.0f, -20.0f, 0.0f};
+    zeroTriangle._v1 = (Coord3){20.0f, 20.0f, 0.0f};
+    zeroTriangle._v2 = (Coord3){0.0f, 20.0f, 0.0f};
 
     while (quit == 0)
     {
@@ -280,15 +524,50 @@ int main(int argc, char *argv[])
             {
                 quit = 1;
             }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_w)
+                {
+                    camera._pos._y += 100.0f;
+                }
+                if (event.key.keysym.sym == SDLK_SPACE)
+                {
+                    FILE *ptr;
+                    ptr = fopen("test.csv","w");
+
+                    fprintf(ptr,"\"x\", \"y\", \"z\"\n");
+                    for (i=0; i<monCube._trianglesSize; ++i)
+                    {
+                        coord = monCube._triangles[i]._v0;
+                        fprintf(ptr,"%.2f, %.2f, %.2f\n", coord._x, coord._y, coord._z);
+
+                        coord = monCube._triangles[i]._v1;
+                        fprintf(ptr,"%.2f, %.2f, %.2f\n", coord._x, coord._y, coord._z);
+
+                        coord = monCube._triangles[i]._v2;
+                        fprintf(ptr,"%.2f, %.2f, %.2f\n", coord._x, coord._y, coord._z);
+                    }
+
+                    fclose(ptr);
+                }
+            }
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         SDL_FillRect(surfaceScreen, NULL, SDL_MapRGB(surfaceScreen->format, 0,0,0));
+        ClearCamera(&camera);
 
         ApplyMatrixToObject(&rotationMatrixZ, &monCube);
         ApplyMatrixToObject(&rotationMatrixX, &monCube);
 
-        DrawObject3D(surfaceScreen, &monCube, &camera);
+        DrawObject3D(&monCube, &camera);
+        DrawTriangle(&testTriangle, &camera);
+        r = 0;
+        g = 255;
+        b = 0;
+        DrawTriangle(&zeroTriangle, &camera);
+
+        ToSurfacePixels(surfaceScreen, &camera);
 
         ++count;
 
